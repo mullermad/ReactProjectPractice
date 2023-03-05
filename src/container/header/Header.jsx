f7d6072c25b3e8e7172f834f4196db8c25317bb9
MulukenDemis <amsaledemismuller@gmail.com>
2023-03-05 12:54:38 +0300

    third commit

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,10 +1,33 @@
 
+
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 import "./header.css";
-import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amaizing with GPT-3 OpenAI
+        </h1>
+        <p>lets bed any for travelling bfdkkjjjhvjkdfzzhvhjxzcvjhvjg bfdk
+          kjjjhvjkdfzzhvhjxzcvjhvjg bfdkkjjjhvjkdfzzhvhjxzc
+          vjhvjhbvjxCzzzzzhvhjxzcvjhvjhbvjxC</p>
+        <div className='gpt3__header-content__input'>
+          <input type="email"  placeholder='your email address '/>
+          <button type='button'>Get started</button>
+           
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='people'/>
+          <p>1,600 people requested accored gvhjvkxjbhkjzbkvjb</p>
+        </div>
+        <div className='gpt3__header-image'>
+          <img src={ai} alt='ai'/>
+        </div>
+      </div></div>
   )
 }
 
